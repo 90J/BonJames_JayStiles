95d0b98d460ffee8f458d7ff9f85ee50e1ce6460
Jay Stiles <JayStiles@quickenloans.com>
2018-10-12 15:57:26 -0400

    submitting

---
+++ BondJames_JayStiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BondJames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //vars
+            string fName;
+            string lName;
+
+            //get user input
+            Console.Write("Please enter a first name: ");
+            fName = Console.ReadLine();
+
+            Console.Write("Please enter a last name: ");
+            lName = Console.ReadLine();
+
+            //spit out swapped names
+            Console.WriteLine(lName + " " + fName);
+        }
+    }
+}
